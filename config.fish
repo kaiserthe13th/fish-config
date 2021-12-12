5468bdda8383df731c8beacc428cb2b5f04d64be
kaiserthe13th <superkerem13@gmail.com>
2021-12-12 20:35:33 +0300

    .

--- config.fish
+++ config.fish
@@ -20,12 +20,12 @@ alias l="ls"
 alias ll="exa --icons -l --group-directories-first"
 alias lla="exa -la --icons --group-directories-first"
 alias la="exa -a --icons --group-directories-first"
-# alias bat="batcat"
+alias bat="batcat"
 alias cd..="cd .."
 alias neofetch="neofetch --ascii /home/kerem/Pictures/viridian-ascii.txt --ascii_colors 7 1 2 2 2 4"
 alias vs="code"
 alias n="nvim"
-alias isd="$HOME/code/py/istandil/istandil"
+alias isd="cd $HOME/code/py/istandil/istandil"
 alias tty-clock="tty-clock -s -c -B -b"
 
 ## Temporary Aliases ##
@@ -34,12 +34,13 @@ alias gün="cd $HOME/code/rs/güneşdoğan"
 ########
 # PATH #
 ########
-set PATH $PATH /home/kerem/node-v14.18.1-linux-x64/bin/
+set -x PATH $PATH /home/kerem/.path/
+set -x PATH $PATH /home/kerem/.path/bin/
 
 ############
 # HOMEBREW #
 ############
-echo 'eval "(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' >> /home/kerem/.profile
+# echo 'eval "(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' >> /home/kerem/.profile
 eval (/home/linuxbrew/.linuxbrew/bin/brew shellenv)
 
 ########
